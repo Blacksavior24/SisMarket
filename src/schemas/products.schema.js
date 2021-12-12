d4aed45119346813639c74ef62115d3a53cd177c
erickgodoy <erickgodoy@gmail.com>
2021-12-12 15:11:20 -0500

    añadido products.router.js products.schema.js products.service.js

--- src/schemas/products.schema.js
+++ src/schemas/products.schema.js
@@ -1,1 +1,18 @@
-console.log('hola ');
+const Joi = require('joi');
+
+const id = Joi.number().integer();
+const name = Joi.string().min(3);
+
+const createProductSchema = Joi.object({
+  name: name.required()
+});
+
+const updateProductSchema = Joi.object({
+  name: name
+});
+
+const getProductSchema = Joi.object({
+  id: id.required(),
+});
+
+module.exports = { createProductSchema, updateProductSchema, getProductSchema }
