d0225ea4b49fbbb0d2e22013b379289a66fc8531
Emerson Villalta <reaperfocs@gmail.com>
2022-01-05 14:39:22 -0500

    actualizando relación y control de ingreso de datos en los productos

--- src/schemas/products.schema.js
+++ src/schemas/products.schema.js
@@ -4,16 +4,19 @@ const id = Joi.number().integer();
 const name = Joi.string().min(3);
 const code = Joi.string().max(13);
 const description = Joi.string().min(5);
+const categoryId = Joi.number().integer();
 
 const createProductSchema = Joi.object({
   name: name.required(),
   code: code.required(),
-  description: description.required()
+  description: description.required(),
+  categoryId: categoryId.required(),
 });
 
 const updateProductSchema = Joi.object({
   name: name,
-  description: description
+  description: description,
+  categoryId: categoryId
 });
 
 const getProductSchema = Joi.object({
