da2101033e6ce07ecf881ed168a4320f3b3ef2fe
erickgodoy <erickgodoy725@gmail.com>
2022-01-07 16:28:45 -0500

    precios de productos en products.schema

--- src/schemas/products.schema.js
+++ src/schemas/products.schema.js
@@ -4,6 +4,8 @@ const id = Joi.number().integer();
 const name = Joi.string().min(3);
 const code = Joi.string().max(13);
 const description = Joi.string().min(5);
+const purchasePrice = Joi.number().precision(2);
+const salePrice = Joi.number().precision(2);
 const categoryId = Joi.number().integer();
 
 const createProductSchema = Joi.object({
