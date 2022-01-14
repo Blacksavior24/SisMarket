59113cf95cf97e1ce61a42e595e65ca780e6d61c
Erick Godoy <erickgodoy725@gmail.com>
2022-01-14 10:11:43 -0500

    create models provider and purchaseorder

--- src/db/models/index.js
+++ src/db/models/index.js
@@ -1,19 +1,28 @@
 const {User, UserSchema} = require('./user.model');
+
 const {Category, CategorySchema} = require('./category.model');
 const {Product, ProductSchema} = require('./product.model');
 const {Inventory, InventorySchema} = require('./inventory.model');
 
+const {Provider, ProviderSchema} = require('./provider.model');
+const {PurchaseOrder, PurchaseOrderSchema} = require('./purchaseorder.model')
 
 function setupModels(sequelize){
   User.init(UserSchema, User.config(sequelize));
+
   Category.init(CategorySchema, Category.config(sequelize));
   Product.init(ProductSchema, Product.config(sequelize));
   Inventory.init(InventorySchema, Inventory.config(sequelize));
 
+  Provider.init(ProviderSchema, Provider.config(sequelize));
+  PurchaseOrder.init(PurchaseOrderSchema, PurchaseOrder.config(sequelize));
 
   Category.associate(sequelize.models);
   Product.associate(sequelize.models);
   Inventory.associate(sequelize.models);
+
+  Provider.associate(sequelize.models);
+  PurchaseOrder.associate(sequelize.models);
 }
 
 
